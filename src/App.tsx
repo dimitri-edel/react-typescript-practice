efd11155eceaa8e2ec302762813fe970828bb547
dimitri-edel <dimitri.edel@gmx.net>
2025-06-12 19:49:02 +0200

    Set up a React Router in App.tsx, then include a route to the home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css'
+import type { IUser } from './types/user';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/home';
 
 function App() {  
-
+  const user: IUser = {
+    username: 'john_doe',
+    email: 'john_doe@some.com',
+    birthdate: new Date('1990-01-01'),
+    locale: 'en-US'
+  };
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home user={user} />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
