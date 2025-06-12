c2ec41e9f3e48dfdae654c7d1924e388a5cae6b9
dimitri-edel <dimitri.edel@gmx.net>
2025-06-12 19:40:12 +0200

    Add type IUser and incorporate it into home.tsx

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,4 +1,18 @@
-function Home() {
+import type { IUser } from '../../types/user';
+
+function Home(props: { user: IUser }) {
+  const { user } = props;
+    if (!user) {
+        return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
+            <p className="text-lg mb-8">Please log in to continue.</p>
+            <a href="/login" className="text-blue-500 hover:underline">
+            Log In
+            </a>
+        </div>
+        );
+    }
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+// src/types/user.ts
+
+export interface IUser {
+  username: string;
+  email: string;
+  birthdate: Date;
+  locale: string;
+}
