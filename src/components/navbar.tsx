c81bf4f97438e9725c09cd9be4c92ce34d710311
dimitri-edel <dimitri.edel@gmx.net>
2025-06-19 14:13:39 +0200

    Add react-page to the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -33,6 +33,7 @@ function NavBar() {
         <Link to="/about" className="text-gray-300 hover:text-gray-400 transition-colors">About</Link>
         <Link to="/i18n-page" className="text-gray-300 hover:text-gray-400 transition-colors">i18n</Link>
         <Link to="/typescript-page" className="text-gray-300 hover:text-gray-400 transition-colors">TypeScript</Link>
+        <Link to="/react-page" className="text-gray-300 hover:text-gray-400 transition-colors">React</Link>
         {isSignedIn ? (
           <button
             onClick={handleLogoutClick}
